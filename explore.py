9908c74b978ad2d3ba3197f3653b4813d7757323
hungqng <97866339+hungqng@users.noreply.github.com>
2022-01-25 15:37:40 -0800

    Add files via upload

--- explore.py
+++ explore.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 
 client = Client(
-    "ACd964ded0fc8ede45d1e4c81d31414eae", 
-    "ccf4a9dea7fbf160f83cff08bc3b5ca4"
+    "TWILIO_ACCOUNT_SID", 
+    "TWILIO_AUTH_TOKEN"
     )
 
 for msg in client.messages.list():
