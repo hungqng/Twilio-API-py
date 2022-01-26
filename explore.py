49e94f0f7b5c335d2e4ef737d507c62cdd8727e3
hungqng <97866339+hungqng@users.noreply.github.com>
2022-01-25 16:02:55 -0800

    Add files via upload
    Added documentation and delete function

--- explore.py
+++ explore.py
@@ -1,15 +1,22 @@
+# Install Twilio packgage
 from twilio.rest import Client
 
+# Input SID and Authorize token here
 client = Client(
     "TWILIO_ACCOUNT_SID", 
     "TWILIO_AUTH_TOKEN"
     )
 
+# Sending message
 message = client.messages.create(
     from_="+16065311900",
     to="+12067246799",
     body="Hello World from Python",
     )
 
+# Deleting message
+#for message in client.messages.list():
+#    print(f"Deleting {message.sid}")
+#    message.delete()
 
 print(f"Created a new message: {message.sid}")
